195fa2d8b615f9c57d6c222d78ccbc86ebfe8a90
Ecstasy <59181303+yourpov@users.noreply.github.com>
2025-07-26 22:59:49 -0400

    Add files via upload

---
+++ Example/example.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/yourpov/gradify"
+)
+
+func main() {
+	fmt.Println(gradify.Gradient("x o x o | Error Preset!   | x o x o", gradify.Error))
+	fmt.Println(gradify.Gradient("x o x o | Success Preset! | x o x o", gradify.Success))
+	fmt.Println(gradify.Gradient("x o x o | Warning Preset! | x o x o", gradify.Warning))
+	fmt.Println(gradify.Gradient("x o x o | Info Preset!    | x o x o", gradify.Info))
+	fmt.Println(gradify.Gradient("x o x o | Candy Preset!   | x o x o", gradify.Candy))
+	fmt.Println(gradify.Gradient("x o x o | Minty Preset!   | x o x o", gradify.Minty))
+}
